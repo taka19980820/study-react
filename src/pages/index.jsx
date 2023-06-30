2f817b66abfc5fae973bd9c736956e706208c661
thorikoshi19980820 <takaya.b.b0820@gmail.com>
2023-07-01 00:35:50 +0900

    クリックイベント学習用

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,29 @@ import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
 import Head from 'next/head'
+import { useCallback } from 'react'
 
 
 
 export default function Home() {
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault();
+    alert(1);
+  }, []);
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a 
+        href="/about"
+        onClick={handleClick}>
+        ボタン
+      </a>
       <Main title="index" />
       <Footer />
     </div>
